d5f2388d71db0ac65069dd4b0ae3ffaf2357387a
SlimNate <slimnatekc@gmail.com>
2025-05-17 02:43:17 -0500

    Add title hero component, and add to home page

---
+++ src/lib/components/TitleHero.svelte
@@ -0,0 +1,23 @@
+<script>
+	let {
+		title = 'Welcome to SvelteKit',
+		subtitle = 'A SvelteKit starter template',
+		heroImage = '/images/hero.jpg',
+		cta_text = 'Book Now',
+		cta_link = '/booking',
+		heroImageHeight = '40vh'
+	} = $props();
+</script>
+
+<div class="hero min-h-[{heroImageHeight}]" style="background-image: url({heroImage});">
+	<div class="hero-overlay"></div>
+	<div class="hero-content text-neutral-content text-center">
+		<div class="max-w-md">
+			<h1 class="mb-5 text-5xl font-bold">{title}</h1>
+			<p class="mb-5">
+				{subtitle}
+			</p>
+			<a href={cta_link}><button class="btn btn-primary">{cta_text}</button></a>
+		</div>
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
+<script>
+	import { name, description } from '$lib/data/meta.js';
+
+	import TitleHero from '$lib/components/TitleHero.svelte';
+</script>
+
+<TitleHero
+	title="Welcome to {name}"
+	subtitle={description}
+	heroImage="https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp"
+	heroImageHeight="40vh"
+/>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
